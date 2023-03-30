f53921b3d5cc0d1556da1b6aa386f25b039062dd
Daryl Bacurin <darylbacurin14@gmail.com>
2023-03-30 17:57:56 +0800

    Test [Jquery scroll]

--- js/index.js
+++ js/index.js
@@ -242,5 +242,5 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 200);
+    }, 100);
 });
